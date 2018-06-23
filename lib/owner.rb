3d7a439dff65cfc5e0f7f217f42486ed298fc2c2
Laura Messa Travin <laura.travin@gmail.com>
2018-06-23 23:06:19 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,6 +1,6 @@
 class Owner
   # code goes here
-  attr_accessor :pets, :name, :fish
+  attr_accessor :pets, :name
   attr_reader :species
   @@all = []
   def initialize(species)
@@ -26,6 +26,10 @@ class Owner
     @pets[:fishes] << fish
   end
   def buy_cat(name)
+    cat = Cat.new(name)
+    @pets[:cats] << cat
+  end
+  def buy_dog(name)
     cat = Cat.new(name)
     @pets[:cats] << cat
   end
