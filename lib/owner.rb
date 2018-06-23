3587beb4bfcdec1ca83a1bd3b7acd8965d5d86ed
Laura Messa Travin <laura.travin@gmail.com>
2018-06-23 23:48:41 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -51,7 +51,7 @@ class Owner
      self.pets[:dogs].clear
   end
   def list_pets
-      text = "I have "+self.pets[:fishes].length.to_s + " " + self.pets[:dogs].length.to_s + " " + self.pets[:cats].length.to_s
+      text = "I have "+self.pets[:fishes].length.to_s + " fish, " + self.pets[:dogs].length.to_s + " dogs(s), " + self.pets[:cats].length.to_s
       return text
   end
 end
